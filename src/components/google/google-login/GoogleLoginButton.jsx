aab1fc93265632728fe85efba26fef5fde15567d
tatarynrm <tatarynrm@gmail.com>
2024-06-13 18:53:23 +0300

    google auth

--- src/components/google/google-login/GoogleLoginButton.jsx
+++ src/components/google/google-login/GoogleLoginButton.jsx
@@ -4,7 +4,8 @@ import { Button } from '@chakra-ui/react';
 import { FcGoogle } from "react-icons/fc";
 const GoogleLoginButton = () => {
 const google  = ()=>{
-    window.open('http://localhost:8800/auth/google','_self')
+    // window.open('https://api.vendwater.tech/auth/google','_self')
+    window.open('https://api.vendwater.tech/auth/google','_blank', 'width=600,height=400')
 }
     return (
 <Button marginTop={'20px'} width={'100%'} rightIcon={<FcGoogle/>} onClick={google}>Увійти з Google  </Button>
